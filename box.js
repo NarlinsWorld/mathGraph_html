554c84e6a8fb3d0949925504d0a480ae9a590f47
NarlinsWorld <drdrugus@gmail.com>
2024-06-20 15:49:31 -0400

    Major features working

--- box.js
+++ box.js
@@ -1,16 +1,110 @@
-class Box{
-    constructor(myscale,xmin,xbox,ymin,ybox){
+class Box {
+    constructor(myscale, xmin, xmax, xbox, ymin, ymax, ybox) {
         this.myscale = myscale;
         this.xmin = xmin;
         this.xbox = xbox;
         this.ymin = ymin;
         this.ybox = ybox;
+        this.xmax = xmax;
+        this.ymax = ymax;
+        this.xdecimals = 1;
+        this.ydecimals = 1;
+        this.fsize = 22;
+
+        this.Middle = createVector(width / 2, height / 2);
+        this.topCornerX = round(this.Middle.x - this.myscale * width / 2);
+        this.topCornerY = round(this.Middle.y - this.myscale * height / 2);
+        this.lowerLeft = createVector(this.topCornerX, height * this.myscale + this.topCornerY); //lower left corner
+        this.rt = createVector(this.lowerLeft.x + this.myscale * width, this.lowerLeft.y); //lowr right corner 
+        this.boxwidth = this.rt.x - this.lowerLeft.x;
+        this.boxheight = this.lowerLeft.y - this.topCornerY;
+        //this.zz = createVector();
+        // this.m = (this.rt.x - this.lowerLeft.x) / (this.xmax - this.xmin);
+        // this.zz.x = this.lowerLeft.x + this.m * (0 - this.xmin);
+        // this.m = (this.topCornerY - this.lowerLeft.y) / (this.ymax - this.ymin);
+        // this.zz.y = this.lowerLeft.y + this.m * (0 - this.ymin);
+        console.log('canvas units for LL=',this.lowerLeft)
     }
 
-    theBox(){
+    canvasUnits(x,y,xmin,xmax,ymin,ymax){
+        //we want canvas units from mathUnits.
+        let x1 = map(x,xmin,xmax,this.lowerLeft.x,this.rt.x);
+        let y1 = map(y,ymin,ymax,this.lowerLeft.y,this.topCornerY);
+        return [x1,y1];
+    }
+
+    thebox(xmin, xmax, xbox, ymin, ymax, ybox) {
+        push();
+        //textFont(tahoma);
+        textSize(this.fsize);
         noFill();
-      stroke('#000000');
-      strokeWeight(3); //applies to lines and string characters
-      rect(this.xmin,this.ymax, width,height);
+        stroke('#000000');
+        strokeWeight(3); //applies to lines and tics
+        rect(this.topCornerX, this.topCornerY, this.boxwidth, this.boxheight); //draws the rectangle
+        fill('#000000'); //fill for tic marks
+
+        //draw the x-axis tic marks using a for loop
+        //These items don't belong in the loop    
+        this.m = (this.rt.x - this.lowerLeft.x) / xbox;
+        textAlign(CENTER, CENTER);
+        let xnlen = numlen(xmin, xmax, xbox); // maximum xnumber length in characters - not used
+
+        for (let i = 0; i < floor(xbox + 1); i++) {
+            let tix = this.lowerLeft.x + this.m * i; //x location to begin tix
+            strokeWeight(3); // Line weight
+            line(tix, this.lowerLeft.y, tix, this.lowerLeft.y - 25 * this.myscale); // the tic line            
+            let xn = xmin + i * (xmax - xmin) / xbox; //calc the tic number
+            if (this.xdecimals != 0) {
+                xn = xn.toFixed(this.xdecimals);
+            } //does user want fixed decimal
+            let xnumber = str(xn); // the string to print   
+            strokeWeight(0); //text weight - we change size, not weight
+            text(xnumber, tix, this.lowerLeft.y + .03 * height); //the printed tic number           
+        } //end for
+
+        //draw the y-axis tic marks
+        //determine the maximum ynumber length
+        textAlign(LEFT, CENTER);
+        let nlen = numlen(ymin, ymax, ybox);
+        this.m = (this.topCornerY - this.lowerLeft.y) / ybox;
+
+        for (let i = 0; i < floor(ybox+1); i++) {
+            let tix = this.lowerLeft.y + this.m * i; //y location to begin tic
+            strokeWeight(3);
+            line(this.lowerLeft.x, tix, this.lowerLeft.x + 25 * this.myscale, tix); //draw the tic
+            let yn = ymin + i * (ymax - ymin) / ybox //calc the tic value
+            if (this.xdecimals != 0) {
+                yn = yn.toFixed(this.ydecimals);
+            } //if user wanted fixed decimals
+            let ynumber = str(yn); //the string to print
+            strokeWeight(0);
+            let lineWidth = textWidth(ynumber);
+            
+            text(ynumber, this.lowerLeft.x - lineWidth*1.2, tix + .000 * height); //the printed tic number
+            
+            
+        } //end for
+        slowdown = false; //only debug print for 1 set.
+
+
+        //console.log('thebox: xmin='+this.xmin+' ymin='+this.ymin+' xmax='+this.xmax+' ymax='+this.ymax);
+        //console.log('thebox: LL=' + x1 + ',' + y1);
+
+        circle(10, 10, 10);
+        pop();
     }
 }
+
+//function to determine the maximum number length for the axes.
+//nmin = xmin or ymin
+//nmax = xmax or ymax
+//numbox = xbox or ybox
+function numlen(nmin, nmax, numbox) {
+    let nlen = str(nmin).length;
+    let mlen = str(nmin + floor(numbox + 1) * (nmax - nmin) / numbox).length;
+    nlen = max(nlen, mlen);
+    if ((nmax - nmin) % numbox === 0 && nlen > 2) {
+        nlen = nlen - 2;
+    }
+    return nlen;
+} // end numlen
--- sketch.js
+++ sketch.js
@@ -6,11 +6,13 @@ Is it worth it?
 
 
 
-let trip = 0; //accumulated in checkInput() in file util.js
+let trip = 0; //accumulated in checkInput() in file util.js - used in debugging
+let slowdown = true; //use for debug
+
 function setup() {
-  cvs = createCanvas(400, 400);
+  cvs = createCanvas(700, 600);
   cvs.parent("cvsdiv");
-  let minmax = checkStartup(); //fills the array with the startup values. Only OK in setup
+  let minmax = checkStartup(); //fills the array with the onscreen startup values. Only OK in setup
   let xmin = minmax[0];
   let xmax = minmax[1];
   let ymin = minmax[2];
@@ -22,9 +24,8 @@ function setup() {
   //create some data
   globalThis.x = []; //globals
   globalThis.y = [];
-  //let numXbox=6; let numYbox = 13;
   grid = new Grid(xmin, xmax, ymin, ymax, numXbox, numYbox);
-
+  box = new Box(.6, xmin, xmax, numXbox, ymin, ymax, numYbox);
   // dxmax = max(x); //minmax for the dataset. Globals
   // dxmin = min(x);
   // dymax = max(y);
@@ -34,22 +35,36 @@ function setup() {
 
 
 function draw() {
-  let obj1 = fetchminmax(); //file is util.js
+  let obj1 = fetchminmax(); //file is util.js the function retrieves onscreen settings
   let xmin = obj1[0];
   let xmax = obj1[1];
   let ymin = obj1[2];
   let ymax = obj1[3];
   let numXbox = obj1[4];
   let numYbox = obj1[5];
-  background(220);
+  background('#d4ffff');
   //translate (xmin,ymin) to the LL corner of box.
-  translate(xzero, yzero);
-  scale(1, -1);
-  expmath(500, xmin, xmax); //subroutine fills arrays x and y and show them. Argument is npts
-  grid.show(); //class grid in in file functionsToPlot.js
-  drawAxes();
+  push();
+  let obj2;
+  obj2 = box.canvasUnits(xmin,0,xmin,xmax,ymin,ymax); //this plots a point exactly correct for scale(1,1)
+//  console.log(box.canvasUnits(0, 0));
 
- 
+fill('red');
+circle(obj2[0], obj2[1], 10);
+noFill;
+
+
+translate(obj2[0], obj2[1]);
+scale(.6)
+scale(1, -1);
+
+expmath(500, xmin, xmax); //subroutine fills arrays x and y and show them. Argument is npts
+//grid.show(); //class grid in in file functionsToPlot.js
+pop();
+
+box.thebox(xmin, xmax, numXbox, ymin, ymax, numYbox);
+
+
   // if (trip == 2) {  //accumulated in checkInput() in file util.js  used to stop console for debugging
   //   console.log('trip =',trip)
   //   noLoop();
